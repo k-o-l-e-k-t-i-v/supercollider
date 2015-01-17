0bb6176aedfb830aa814cfa320075b25ed0a141d
k0f <krystof.pesek@gmail.com>
2015-01-17 11:59:36 +0100

    sync by annex @ Sat Jan 17 11:59:36 CET 2015

--- kof_local_sync.sh
+++ kof_local_sync.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 git add .
 git commit -am "sync by annex @ `date`"
+
+echo "//////////// syncing annex folder ////////////////////////////////////"
 cd ~/annex && git annex sync
+echo "//////////// syncing Dropbox folder ////////////////////////////////////"
 cd ~/Dropbox/pd/Supercollider/GIT && git annex sync
+echo "//////////// syncing ownCloud folder ////////////////////////////////////"
 cd ~/ownCloud/public/supercollider && git annex sync
