0bb98c0ba2528e9066c610386bddcd96a45c5e47
k0f <krystof.pesek@gmail.com>
2015-02-28 20:25:39 +0100

    sync /w annex @ Sat Feb 28 20:25:39 CET 2015

---
+++ kof_local_sync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#test watch
+
+echo "//////////// generating statistics ////////////////////////////////////"
+cd ~/annex/utils/lang_statistics
+./stat.sh
+gnuplot plot.gp
+
+echo "//////////// syncing annex folder ////////////////////////////////////"
+cd ~/annex
+git add .
+git commit -am "sync /w annex @ `date`"
+git annex sync
+
+echo "//////////// syncing Dropbox & GIT folder ////////////////////////////////////"
+cd ~/Dropbox/pd/Supercollider/GIT
+git add .
+git commit -am "sync /w Dropbox @ `date`"
+git annex sync
+
+echo "//////////// syncing ownCloud folder ////////////////////////////////////"
+cd ~/ownCloud/public/supercollider
+git add .
+git commit -am "sync /w ownCloud @ `date`"
+git annex sync
+
+echo "//////////// syncing annex folder ////////////////////////////////////"
+cd ~/annex
+git annex sync
+
+
